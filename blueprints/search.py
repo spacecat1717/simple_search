05be9f09be804299c80fda4b5c98b13eda3395e9
spacecat1717 <manyam245@gmail.com>
2023-03-07 23:16:00 +0300

    fix stupid bug

--- blueprints/search.py
+++ blueprints/search.py
@@ -9,10 +9,11 @@ do_search = Blueprint('search', __name__)
 @do_search.get('/')
 async def search() -> tuple:
     """Blueprint for search request"""
+    if request.args.get('q') == '':
+        return {"status": "error", "type": "incorrect request"}, 400
     query = request.args.get('q')
     es = ElasticManager()
     db = RecordManager()
     record_ids = await es.search(query)
-    print(record_ids)
     result = await db.get_records_by_search(record_ids)
     return {"status": "ok", "data": result}, 200
